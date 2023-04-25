b30f0ae4fd0eb71f6374f2b8ef48578ba88899cf
palomagt05 <gatitogt919@gmail.com>
2023-04-25 10:07:56 -0600

    Template AdminLTE agregado

--- application/controllers/Taller.php
+++ application/controllers/Taller.php
@@ -3,6 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Taller extends CI_Controller {
 
+	public function __construct()
+	{
+		//Hereda los métodos de la super clase CI_Controles
+		parent::__construct();
+		//Agregar helpers y librerias adicionales al controlador 
+		$this->load->helper('url');
+	}
+
 	public function index()
 	{
 		$this->load->view('Taller/index_view');
@@ -14,5 +22,9 @@ class Taller extends CI_Controller {
 	public function agregar_taller()
 	{
 		$this->load->view('Taller/agregar_taller_view');
+	}
+	public function plantilla()
+	{
+		$this->load->view('Taller/blank_view');
 	}
 }
