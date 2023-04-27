2b33b6ef65bcfa9dc17d4648569693e01d29cd96
palomagt05 <gatitogt919@gmail.com>
2023-04-26 21:51:42 -0600

    Carpeta commons

---
+++ application/views/Commons/javascript_view.php
@@ -0,0 +1,17 @@
+<!-- jQuery -->
+<script src="<?php echo base_url('vendor/plugins/jquery/jquery.min.js') ?>"></script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo base_url('vendor/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url('vendor/dist/js/adminlte.min.js') ?>"></script>
+
+<!-- Datatable -->
+<script src="<?php echo base_url('vendor/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-responsive/js/dataTables.responsive.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-buttons/js/dataTables.buttons.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-buttons/js/buttons.html5.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-buttons/js/buttons.print.min.js') ?>"></script>
+<script src="<?php echo base_url('vendor/plugins/datatables-buttons/js/buttons.colVis.min.js') ?>"></script>
